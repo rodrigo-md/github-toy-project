d752a3b3c6b8fc68bb5b1eeb01b0265c6fc189dd
Rodrigo <martinez.d.rodrigo@gmail.com>
2022-02-15 23:50:41 -0300

    Update sum.spec.js

--- src/sum.spec.js
+++ src/sum.spec.js
@@ -2,7 +2,7 @@ const  {describe, test, expect } = require('@jest/globals');
 const { sum } = require('./sum');
 
 describe('sum', function () {
-    test('sum 1 and 2 returns 3', function () {
+    test('take 1 and 2 and returns 3', function () {
         expect(sum(1,2)).toEqual(3);
     })
 })
